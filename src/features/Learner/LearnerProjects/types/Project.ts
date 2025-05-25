070349d1c7b6041c0f3d930e08f11b33dd31ffe9
ShaminiNirodyaExcelly <nirodya@excellygen.io>
2025-05-25 14:57:52 +0530

    EXGLMS-27: Added Learner Project View -n

--- src/features/Learner/LearnerProjects/types/Project.ts
+++ src/features/Learner/LearnerProjects/types/Project.ts
@@ -1,14 +1,17 @@
+// Path: src/features/Learner/LearnerProjects/types/Project.ts
+// THIS FILE WILL NOT BE CHANGED
+
 export type ProjectStatus = 'Assigned' | 'Completed';
 
 export interface Project {
-  id: number;
+  id: number; // Existing type expects a number
   title: string;
   status: ProjectStatus;
   description: string;
-  team: string[];
-  role?: string; 
+  team: string[]; // Existing type expects this
+  role?: string;
   startDate: string;
-  endDate?: string;
-  completionDate?: string;
+  endDate?: string; // For 'Assigned' status
+  completionDate?: string; // For 'Completed' status
   technologies: string[];
 }
